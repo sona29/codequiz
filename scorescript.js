7c4f254c4c433586a0739e8226cca99975d39876
Sona Shrestha <sona.shrestha004@gmail.com>
2021-03-25 08:50:31 +1100

    adding fuctionality

--- scorescript.js
+++ scorescript.js
@@ -3,13 +3,14 @@ var noScore = document.querySelector(".no-highscore");
 var playButton = document.querySelector(".play-button");
 
 var backButton = document.getElementById("play-quiz");
+var lastScore = [];
 
 // run event when page loads
 document.addEventListener('DOMContentLoaded', function() {
-var lastScore = JSON.parse(localStorage.getItem("highScores"));
+    var lastScore = JSON.parse(localStorage.getItem("highScores")) || [];
   
 
-    if(lastScore[0].length != 0){        
+    if(lastScore.length){        
         for (var i = 0; i < lastScore.length; i++)
             {
                 console.log(lastScore[i].score);
@@ -23,15 +24,12 @@ var lastScore = JSON.parse(localStorage.getItem("highScores"));
    
         else{
         noScore.textContent = 'No scores yet';
-        var button = document.createElement("button");
-        button.innerHTML = "Play quiz";
-        button.id = "play-quiz";
-        playButton.append(button);
+        
             }
 
 }, false);
 
 backButton.addEventListener("click",function(event){
-    alert("here");
+    document.location.href = "index.html";
 });
 
